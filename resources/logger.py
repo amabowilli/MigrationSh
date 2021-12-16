8b9dd7bd2d4defbe95f34d78a6bcac8010a7b99b
michael walker <miwalker@atlassian.com>
2021-12-16 11:09:23 -0600

    switched logger to WARN on terminal and full debug in log file

--- resources/logger.py
+++ resources/logger.py
@@ -5,10 +5,10 @@ log_formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
 file_handler = logging.FileHandler('mirror-groups.log')
 file_handler.setFormatter(log_formatter)
-file_handler.setLevel(logging.WARNING)
+file_handler.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.WARNING)
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
